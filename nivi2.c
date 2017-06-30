cf651243c39f061ef3a6f74d1a3539fa19b583fb
nivethanakul <nivimurugesan05@gmail.com>
2017-06-30 11:11:10 +0530

    Create nivi2.c

---
+++ nivi2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the number:");
+scanf("%d",&a);
+if(a%2==0)
+printf("\n the number is even");
+else
+printf("\n the number is odd");
+getch();
+}
